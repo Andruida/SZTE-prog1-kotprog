e38c86f951aead45f076dbbec6ad3b01a89ac604
Andruida <borus.benedek@gmail.com>
2023-03-19 23:44:50 +0100

    Update Inventory.java

--- prog1/kotprog/dontstarve/solution/inventory/Inventory.java
+++ prog1/kotprog/dontstarve/solution/inventory/Inventory.java
@@ -6,16 +6,60 @@ import prog1.kotprog.dontstarve.solution.inventory.items.ItemType;
 
 public class Inventory implements BaseInventory {
 
-    private AbstractItem[] slots = new AbstractItem[10];
+    private AbstractItem[] slots;
 
     public Inventory() {
-        
+        slots = new AbstractItem[10];
     }
 
     @Override
-    public boolean addItem(AbstractItem item) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'dropItem'");
+    public boolean addItem(final AbstractItem _item) {
+        if (_item == null) {
+            return false;
+        }
+
+        AbstractItem item = _item.clone();
+        if (item.getAmount() <= 0) {
+            return true;
+        }
+
+        // van-e ugyanolyan típusú item?
+        for (int i = 0; i < slots.length; i++) {
+            if (slots[i] == null || slots[i].getType() != item.getType() || 
+                slots[i].getAmount() == slots[i].getMaxStackAmount()) {
+                    continue;
+            }
+            
+            if (slots[i].getAmount() + item.getAmount() <= slots[i].getMaxStackAmount()) {
+                slots[i].addAmount(item.getAmount());
+                return true;
+            }
+
+            int amount = slots[i].getMaxStackAmount() - slots[i].getAmount();
+            slots[i].addAmount(amount);
+            AbstractItem newItem = item.clone();
+            newItem.addAmount(-amount);
+            return this.addItem(newItem);
+        }
+
+        // van-e üres slot?
+        for (int i = 0; i < slots.length; i++) {
+            if (slots[i] != null) {
+                continue;
+            }
+            if (item.getAmount() <= item.getMaxStackAmount()) {
+                slots[i] = item;
+                return true;
+            }
+            int amount = item.getMaxStackAmount();
+            AbstractItem newItem = item.clone();
+            item.setAmount(amount);
+            slots[i] = item;
+            newItem.addAmount(-amount);
+            return this.addItem(newItem);
+        }
+
+        return false;
     }
 
     @Override
@@ -86,8 +130,7 @@ public class Inventory implements BaseInventory {
 
     @Override
     public AbstractItem getItem(int index) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getItem'");
+        return slots[index];
     }
     
 }
